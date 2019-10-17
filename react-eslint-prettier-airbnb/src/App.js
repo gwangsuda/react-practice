9278ba2b769a821fe65c921dad3d29de1428326c
gwangsuda <gwangsuda@gmail.com>
2019-10-17 09:51:05 +0900

    eslint + prettier + airbnb

---
+++ react-eslint-prettier-airbnb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome mesg="Pengsu" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-eslint-prettier-airbnb/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = ({ mesg }) => (
+  <div>
+    <h1>
+      Hello
+      {mesg}
+    </h1>
+  </div>
+);
+
+export default Welcome;
